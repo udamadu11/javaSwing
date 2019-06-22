eddb5dc5d25f0f9d6b5edfa11b5c9c4b7b42edab
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-06-23 04:19:00 +0530

    makeInterce

--- swing.java
+++ swing.java
@@ -4,22 +4,25 @@ public class swing{
 		JFrame jf = new JFrame("Swing Example");
 
 		JLabel jb = new JLabel("UserName");
-		jb.setBounds(20,70,80,100);
+		jb.setBounds(20,70,70,20);
 
 		JLabel jb2 = new JLabel("password");
-		jb2.setBounds(20,100,80,100);
+		jb2.setBounds(20,100,70,20);
 
 		JTextField jt = new JTextField();
-		jt.setBounds(100,70,50,50);
+		jt.setBounds(100,70,70,20);
 
 		JPasswordField jp = new JPasswordField();
-		jp.setBounds(100,100,50,50);
+		jp.setBounds(100,100,70,20);
 
+		JButton j = new JButton("Login");
+		j.setBounds(100,130,70,20);
 
 		jf.add(jb);
 		jf.add(jb2);
 		jf.add(jt);
 		jf.add(jp);
+		jf.add(j);
 		jf.setSize(500,500);
 		jf.setLayout(null);
 		jf.setVisible(true);
